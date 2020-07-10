17b70ad34682ef2cf16c2b5a2008e951709d7630
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-07-10 19:18:04 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -78,9 +78,8 @@ function renderToys(toy) {
 }
 
 
-// add listener to 'Add Toy' button to show or hide form
 addBtn.addEventListener('click', () => {
-  // hide & seek with the form
+
   addToy = !addToy
   if (addToy) {
     toyForm.style.display = 'block'
@@ -93,11 +92,10 @@ addBtn.addEventListener('click', () => {
   }
 })
 
-// start by getting all toys
 
 getToys().then(toys => {
   toys.forEach(toy => {
-    //function to render toys goes here or something
+  
     renderToys(toy)
   })
 })
